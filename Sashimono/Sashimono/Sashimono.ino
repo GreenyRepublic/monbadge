71f562260dcdf7bc354a147a0c9d8512d2ab53cb
GreenyRepublic <benfclark@urog.co.uk>
2016-03-03 01:48:32 +0000

    First commit of the 'Sashimono' software for the MON badge. (Nothing here at the moment, just getting the project directory synchronised).

---
+++ Sashimono/Sashimono/Sashimono.ino
@@ -0,0 +1,18 @@
+/*
+ Name:		Sashimono
+ Created:	3/3/2016 1:44:42 AM
+ Author:	Benjamin Clark
+ Description: The basic operating software for the MON smart identity badge.
+ Version 0.0.1
+*/
+
+// Initial power-on and reset button code.
+void setup() 
+{
+	
+}
+
+// Main loop.
+void loop() {
+  
+}
